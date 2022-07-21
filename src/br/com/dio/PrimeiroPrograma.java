f24567a200f71f8e5bdb371b3fb2c6c5941afd21
lNotini <lucasnotini@hotmail.com>
2022-07-21 09:26:18 -0300

    Primeiros testes no Intellij

---
+++ src/br/com/dio/PrimeiroPrograma.java
@@ -0,0 +1,13 @@
+package br.com.dio;
+
+import java.math.RoundingMode;
+
+public class PrimeiroPrograma {
+    public static void main(String[] args) {
+
+        double a = 2;
+        int b = 3;
+
+        System.out.println((a*2));
+    }
+}
